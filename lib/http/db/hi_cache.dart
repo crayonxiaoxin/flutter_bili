0d41aee68101f5ea8467897f60937631d1e171d7
Lau <lau@efortunetech.com>
2022-01-05 14:08:01 +0800

    add sp

--- lib/http/db/hi_cache.dart
+++ lib/http/db/hi_cache.dart
@@ -1,3 +1,67 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class HiCache {
-  HiCache._();
+  SharedPreferences? prefs;
+
+  HiCache._() {
+    init();
+  }
+
+  static HiCache? _instance;
+
+  HiCache._pre(SharedPreferences prefs) {
+    this.prefs = prefs;
+  }
+
+  /// 预初始化，防止 get 时还没有初始化完成
+  static Future<HiCache> preInit() async {
+    if (_instance == null) {
+      var prefs = await SharedPreferences.getInstance();
+      _instance = HiCache._pre(prefs);
+    }
+    return _instance!;
+  }
+
+  static HiCache getInstance() {
+    if (_instance == null) {
+      _instance = HiCache._();
+    }
+    return _instance!;
+  }
+
+  void init() async {
+    if (prefs == null) {
+      // 这里是 future 类型的，可能第一次init时，prefs还未初始化成功，所以需要预初始化
+      prefs = await SharedPreferences.getInstance();
+    }
+  }
+
+  HiCache setBool(String key, bool value) {
+    prefs?.setBool(key, value);
+    return this;
+  }
+
+  HiCache setInt(String key, int value) {
+    prefs?.setInt(key, value);
+    return this;
+  }
+
+  HiCache setDouble(String key, double value) {
+    prefs?.setDouble(key, value);
+    return this;
+  }
+
+  HiCache setString(String key, String value) {
+    prefs?.setString(key, value);
+    return this;
+  }
+
+  HiCache setStringList(String key, List<String> value) {
+    prefs?.setStringList(key, value);
+    return this;
+  }
+
+  T? get<T>(String key) {
+    return prefs?.get(key) as T?;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bili/http/core/hi_net.dart';
-import 'package:flutter_bili/http/request/test_request.dart';
+
+import 'http/db/hi_cache.dart';
 
 void main() {
+  HiCache.preInit(); // 初始化 cache
   runApp(MyApp());
 }
 
@@ -51,50 +52,33 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() async {
-    var request = TestRequest();
-    request
-        .addParam("aa", "ddd")
-        .addParam("bb", "333")
-        .addParam("requestPrams", "abc");
-    var result = await HiNet.getInstance().fire(request);
-    print(result);
+    // var request = TestRequest();
+    // request
+    //     .addParam("aa", "ddd")
+    //     .addParam("bb", "333")
+    //     .addParam("requestPrams", "abc");
+    // var result = await HiNet.getInstance().fire(request);
+    // print(result);
+    test2();
     setState(() {
       _counter++;
     });
   }
 
+  void test2() {
+    HiCache.getInstance().setString("bb", "hello");
+    var value = HiCache.getInstance().get("bb");
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
