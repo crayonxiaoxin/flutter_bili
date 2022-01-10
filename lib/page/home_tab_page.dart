ed8ea6e91d6d981349da0a9c1830915f1ece836e
Lau <lau@efortunetech.com>
2022-01-10 18:08:13 +0800

    6-6

--- lib/page/home_tab_page.dart
+++ lib/page/home_tab_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bili/model/home_entity.dart';
 import 'package:flutter_bili/widget/hi_banner.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeTabPage extends StatefulWidget {
   final String name;
@@ -16,17 +17,37 @@ class HomeTabPage extends StatefulWidget {
 class _HomeTabPageState extends State<HomeTabPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        padding: EdgeInsets.only(top: 12),
-        children: [if (widget.bannerList != null) _banner()],
+    return MediaQuery.removePadding(
+      removeTop: true,
+      context: context,
+      // child: ListView(
+      //   children: [if (widget.bannerList != null) _banner()],
+      // ),
+      child: StaggeredGrid.count(
+        crossAxisCount: 2,
+        axisDirection: AxisDirection.down,
+        children: [
+          if (widget.bannerList != null)
+            StaggeredGridTile.count(
+                crossAxisCellCount: 2, mainAxisCellCount: 1, child: _banner()),
+          StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 1,
+              child: Text(
+                "123",
+              )),
+          StaggeredGridTile.count(
+              crossAxisCellCount: 1, mainAxisCellCount: 1, child: Text("236")),
+          StaggeredGridTile.count(
+              crossAxisCellCount: 1, mainAxisCellCount: 2, child: Text("237"))
+        ],
       ),
     );
   }
 
   _banner() {
     return Padding(
-      padding: EdgeInsets.only(left: 18, right: 18),
+      padding: EdgeInsets.only(left: 8, right: 8),
       child: HiBanner(
         widget.bannerList,
         bannerHeight: 150,
